525aa66f8bbf70297c05fa83aab3d609ac09689d
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2024-05-20 19:50:22 -0400

    merge master

---
+++ dist/assets/modalAcceptAution-44d3da93.js
@@ -0,0 +1,1 @@
+import{R as o}from"./index-99830226.js";const e=o("modalAcceptAution",{state:()=>({isActive:!1,dataAutiont:null}),actions:{openModal({isActive:t,data:i}){this.isActive=t,this.dataAutiont=i},closeModal(t){this.isActive=t}}});export{e as M};
