fcf312fe0f62988b05ecafc6a9fec97b81806840
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2024-05-23 14:58:06 -0400

    merge master

---
+++ dist/assets/modalAutoBid-c411c335.js
@@ -0,0 +1,1 @@
+import{R as a}from"./index-1b9cca95.js";const o=a("modalAutoBid",{state:()=>({isActive:!1,data:null}),actions:{openModal(t){this.isActive=t.active,this.data=t.data},closeModal(t){this.isActive=t,this.data=null}}});export{o as M};
