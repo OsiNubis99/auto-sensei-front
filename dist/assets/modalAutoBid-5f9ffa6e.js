8aaabb10bd88efca977ca4875b3e2f22d5cf22df
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2024-10-02 09:24:59 -0400

    feat: Prod build

---
+++ dist/assets/modalAutoBid-5f9ffa6e.js
@@ -0,0 +1,1 @@
+import{P as a}from"./index-e8915cde.js";const o=a("modalAutoBid",{state:()=>({isActive:!1,data:null}),actions:{openModal(t){this.isActive=t.active,this.data=t.data},closeModal(t){this.isActive=t,this.data=null}}});export{o as M};
