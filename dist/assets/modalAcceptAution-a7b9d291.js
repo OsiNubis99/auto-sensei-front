92fcd1326a7d326afa04b1ab90f098d92db0ea08
kennysalaz <mipto.kenny@gmail.com>
2024-10-02 11:57:14 -0400

    update develop

--- dist/assets/modalAcceptAution-a7b9d291.js
+++
@@ -1,1 +0,0 @@
-import{P as o}from"./index-b892a15e.js";const e=o("modalAcceptAution",{state:()=>({isActive:!1,dataAutiont:null}),actions:{openModal({isActive:t,data:i}){this.isActive=t,this.dataAutiont=i},closeModal(t){this.isActive=t}}});export{e as M};
--- dist/assets/modalAutoBid-98d5246c.js
+++
@@ -1,1 +0,0 @@
-import{P as a}from"./index-b892a15e.js";const o=a("modalAutoBid",{state:()=>({isActive:!1,data:null}),actions:{openModal(t){this.isActive=t.active,this.data=t.data},closeModal(t){this.isActive=t,this.data=null}}});export{o as M};
