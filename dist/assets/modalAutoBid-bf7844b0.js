ac84c175102627604b368ba44f74d3434f726b98
KennySalazar12 <ksalazar@mercuriosoft.com>
2025-04-03 17:03:02 -0400

    update/develop

---
+++ dist/assets/modalAutoBid-bf7844b0.js
@@ -0,0 +1,1 @@
+import{P as a}from"./index-cb261f5f.js";const o=a("modalAutoBid",{state:()=>({isActive:!1,data:null}),actions:{openModal(t){this.isActive=t.active,this.data=t.data},closeModal(t){this.isActive=t,this.data=null}}});export{o as M};
