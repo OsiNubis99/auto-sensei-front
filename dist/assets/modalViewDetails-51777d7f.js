305bb56e428089ee658996b73485a25151c8678d
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2025-05-20 17:10:11 -0400

    feat: Build prod

--- dist/assets/modalViewDetails-51777d7f.js
+++
@@ -1,1 +0,0 @@
-import{P as a}from"./index-87c3f2da.js";const s=a("modalViewDetails",{state:()=>({isActive:!1,dataAutiont:null}),actions:{openModal({isActive:t,data:i}){this.isActive=t,this.dataAutiont=i},closeModal(t){this.isActive=t}}});export{s as M};
